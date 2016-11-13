64c28d82136a91187f90ff29035ccba746c90a24
Uriy Kurnosov <teisintai@gmail.com>
2016-11-13 15:03:30 +0300

    Уроки 20-25

---
+++ p0211_twoactivity/src/main/java/startandroid/ru/p0211_twoactivity/ActivityTwo.java
@@ -0,0 +1,18 @@
+package startandroid.ru.p0211_twoactivity;
+
+/**
+ * Created by Redo on 13.11.2016.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ActivityTwo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two);
+    }
+}
